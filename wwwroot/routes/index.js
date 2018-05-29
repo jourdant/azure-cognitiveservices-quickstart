35337d4b4d42b4a1b7cf7f7ae42532e6542f96b1
Jourdan Templeton <jourdan.templeton@awaregroup.com>
2018-05-30 01:24:31 +1200

    Added webcam support

---
+++ wwwroot/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Microsoft Azure + Cognitive Services' });
+});
+
+module.exports = router;
